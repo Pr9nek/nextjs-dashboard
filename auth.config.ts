ccc132faeec4578a8b45cb7ca9d40fc3cfb0a482
Alex <pr9nek@mail.ru>
2025-12-16 23:59:42 +0400

    feat: add logout

--- auth.config.ts
+++ auth.config.ts
@@ -7,6 +7,9 @@ export const authConfig = {
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
+
+            // console.log(`Middleware check for ${nextUrl.pathname}: Is Logged In? ${isLoggedIn}`);
+
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
             if (isOnDashboard) {
                 if (isLoggedIn) return true;
