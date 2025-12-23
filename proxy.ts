569cdddc0687478ea929c228512a964da65529b0
Alex <pr9nek@mail.ru>
2025-12-24 00:04:30 +0400

    feat: add Metadata

--- proxy.ts
+++ proxy.ts
@@ -4,6 +4,10 @@ import { authConfig } from './auth.config';
 export default NextAuth(authConfig).auth;
 
 export const config = {
-    // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
-    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+    // https://nextjs.org/docs/app/building-your-application/routing/proxy#matcher
+    
+    matcher: 
+    [
+        '/((?!api|_next/static|_next/image|.*\\.png$).*)'
+    ],
 };
